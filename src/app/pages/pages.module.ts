c594e903c25968744024959870d136c9906e9c59
Sergio Salinas <sergio-salinas@outlook.com>
2021-03-12 20:53:52 +0100

    Fin seccion 4 - Router

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+
+import { RouterModule } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ]
+})
+export class PagesModule { }
